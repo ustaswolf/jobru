95592b861211abe98bbb293b4606cfb466e21e58
Vitaliy <ustaswolf@narod.ru>
2013-11-10 16:09:41 +0400

    alfa version

---
+++ js/autocomplete.js
@@ -0,0 +1,72 @@
+(function($){
+    $.fn.autoComplete = function( options ){
+        var settings = $.extend({
+            // These are the defaults.
+            items: []
+        }, options );
+
+
+        return this.each(function() {
+            var $this = $(this);
+            var $input = $this.find(".autocomplete");
+            var $span = $this.find("span");
+            var emptyInputSpanVal = $span.text();
+
+            $input.on("focus", function(){
+                $span.html("");
+            });
+            $input.on("blur", function(){
+                if($input.val().length == 0){
+                    $span.html(emptyInputSpanVal);
+                }else{
+                    $span.html("");
+                }
+            });
+
+            $span.click(function(){
+                $input.focus();
+                return false;
+            });
+
+            $input.keyup(function(I){
+                // определяем какие действия нужно делать при нажатии на клавиатуру
+                switch(I.keyCode) {
+                    // игнорируем нажатия на эти клавишы
+                    case 13:  // enter
+                    case 27:  // escape
+                    case 38:  // стрелка вверх
+                    case 40:  // стрелка вниз
+                        break;
+                    case 8:  // backspace
+                        $span.html("");
+                        break;
+                    case 39:  // стрелка вправо
+                        var t = $span.text();
+                        if(t.length > 0){
+                            $input.val(t);
+                            $span.html("");
+                        }
+                        break;
+                    default:
+                        // производим поиск только при вводе более 2х символов
+                        var val = $input.val();
+                        if(val.length > 1){
+                            $span.html("");
+                            var lc = val.toLowerCase();
+                            var i = settings.items.length;
+                            while(i--){
+                                var e = settings.items[i];
+                                if(e.substr(0, lc.length) == lc){
+                                    var h = "<span class='hidden'>" + val.substr(0, lc.length) + "</span>";
+                                    h += e.substr(lc.length);
+                                    $span.html(h);
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                }
+            });
+        });
+    };
+}(jQuery));
---
+++ js/index.js
@@ -0,0 +1,104 @@
+$(function(){
+    $('.userpic').fileapi({
+        url: 'http://rubaxa.org/FileAPI/server/ctrl.php',
+        autoUpload: true,
+        accept: 'image/*',
+        multiple: false,
+        maxSize: FileAPI.MB*10, // max file size
+        imageSize: {
+        	minWidth: 80, 
+        	minHeight: 80 
+        },
+		elements: {
+			active: { 
+				show: '.js-upload', 
+				hide: '.js-browse' 
+			},
+			preview: {
+				el: '.js-preview',
+				width: 80,
+				height: 80
+			},
+			progress: '.js-progress'
+		},
+		onSelect: function (evt, ui){
+			var file = ui.files[0];
+			if( file ){
+				$('#popup').modal({
+					closeOnEsc: false,
+					closeOnOverlayClick: false,
+					onOpen: function (overlay){
+						$(overlay).on('click', '.js-upload', function (){
+							$.modal().close();
+							$('.userpic').fileapi('upload');
+						});
+						$('.js-img', overlay).cropper({
+							file: file,
+							bgColor: '#fff',
+							maxSize: [$(window).width()-100, $(window).height()-100],
+							minSize: [80, 80],
+							selection: '90%',
+							aspectRatio: 1,
+							onSelect: function (coords){
+								$('.userpic').fileapi('crop', file, coords);
+							}
+						});
+					}
+				}).open();
+			}
+		}
+    });
+
+	var year = document.getElementById('year');
+	var month = document.getElementById('month');
+	var day = document.getElementById('day');
+    function daysInMonth(month,year) {
+		var dd = new Date(year, month, 0);
+		return dd.getDate();
+	}
+	
+	function setDayDrop() {
+		var y = year.options[year.selectedIndex].value;
+		var m = month.options[month.selectedIndex].value;
+		var d = day.options[day.selectedIndex].value;
+		if (d == ' ') {
+			var days = (y == ' ' || m == ' ') ? 31 : daysInMonth(m,y);
+			day.options.length = 0;
+			day.options[day.options.length] = new Option(' ',' ');
+			for (var i = 1; i <= days; i++){
+				day.options[day.options.length] = new Option(i,i);
+			}
+		}
+	}
+
+	year.onchange = setDayDrop;
+	month.onchange = setDayDrop;
+
+	$(".js-city").autoComplete({items: ["москва", "питер", "aстрахань", "владивосток"]});
+	$(".js-metro").autoComplete({items: ["комсомольская", "выхино", "пушкинская", "aвиамоторная", "сокол", "аэропорт"]});
+    $(".styled-select select").customSelect();
+    $(".js-add-patronymic").click(function(){
+    	$(this).hide();
+    	var $patronymic = $(".js-patronymic");
+    	$patronymic.slideDown('fast', function(){
+    		$(this).find("input").focus();
+    	});
+    	return false;
+    });
+
+    $(".js-add-phone").click(function(){
+    	var $phones = $(".js-phones");
+
+    	var $phone = $('<input style="display:none" type="text" class="rounded-input" placeholder="Телефон"/>');
+    	$phone.appendTo($phones);
+    	$phone.slideDown("fast", function(){
+    		$(this).focus();
+    	});
+    	return false;
+    });
+
+    $(".js-finish").click(function(){
+    	alert("todo :)");
+    	return false;
+    });
+});
